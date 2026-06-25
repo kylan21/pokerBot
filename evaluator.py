efd554b96f6a8565f58f2e95f6ff1d0aec195f44
kylanli <kylanyli20@gmail.com>
2026-06-25 14:21:37 -0400

    imported treys class. did not realize that card/deck are included so deleted my version lol.

--- evaluator.py
+++ evaluator.py
@@ -1,3 +1,11 @@
-from treys import Evaluator
+from treys import Deck, Evaluator, Card
 
-evaluator = Evaluator()
+deck = Deck()
+
+board = deck.draw(5)
+player1_hand = deck.draw(2)
+player2_hand = deck.draw(2)
+
+Card.print_pretty_cards(board)
+Card.print_pretty_cards(player1_hand)
+Card.print_pretty_cards(player2_hand)
--- test_card.py
+++
@@ -1,6 +0,0 @@
-from card import Card
-
-def test_card_creation():
-    card = Card("A", "s")
-    assert card.suit == "s"
-    assert card.rank == "A"
