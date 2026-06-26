2e975a4dd245acb20fdab1886ec2845990a3418a
kylanli <kylanyli20@gmail.com>
2026-06-26 10:55:08 -0400

    restructuring

--- evaluator.py
+++ evaluator.py
@@ -1,6 +1,7 @@
 from treys import Deck, Evaluator, Card
 
 deck = Deck()
+evaluator = Evaluator()
 
 board = deck.draw(5)
 player1_hand = deck.draw(2)
@@ -9,3 +10,6 @@ player2_hand = deck.draw(2)
 Card.print_pretty_cards(board)
 Card.print_pretty_cards(player1_hand)
 Card.print_pretty_cards(player2_hand)
+
+hands = [player1_hand, player2_hand]
+evaluator.hand_summary(board, hands)
---
+++ game.py
@@ -0,0 +1,73 @@
+from treys import Deck, Card
+from evaluator import Evaluator
+from betting import BettingRound
+from player import Player
+from computer import Computer
+
+class Game:
+    def __init__(self):
+        self.evaluator = Evaluator()
+        self.player = Player(stack=1000) # start w 500bbs
+        self.computer = Computer(stack=1000)
+
+    def play_hand(self):
+        deck = Deck()
+        board = []
+
+        # deal
+        player_hand = deck.draw(2)
+        computer_hand = deck.draw(2)
+
+        print("\n--- YOUR HAND ---")
+        Card.print_pretty_cards(player_hand)
+
+        # preflop
+        result = BettingRound(self.player, self.computer).run()
+        if result == "player_fold": print("You folded"); return
+        if result == "computer_fold": print("Computer folded, you win"); return
+
+        # flop
+        board += deck.draw(3)
+        print("\n--- FLOP ---")
+        Card.print_pretty_cards(board)
+        result = BettingRound(self.player, self.computer).run()
+        if result == "player_fold": print("You folded"); return
+        if result == "computer_fold": print("Computer folded, you win"); return
+
+        # turn
+        board += deck.draw(1)
+        print("\n--- TURN ---")
+        Card.print_pretty_cards(board)
+        result = BettingRound(self.player, self.computer).run()
+        if result == "player_fold": print("You folded"); return
+        if result == "computer_fold": print("Computer folded, you win"); return
+
+        # river
+        board += deck.draw(1)
+        print("\n--- RIVER ---")
+        Card.print_pretty_cards(board)
+        result = BettingRound(self.player, self.computer).run()
+        if result == "player_fold": print("You folded"); return
+        if result == "computer_fold": print("Computer folded, you win"); return
+
+        # showdown
+        self.showdown(player_hand, computer_hand, board)
+
+    def showdown(self, player_hand, computer_hand, board):
+        p_score = self.evaluator.evaluate(player_hand, board)
+        c_score = self.evaluator.evaluate(computer_hand, board)
+
+        print("\n--- SHOWDOWN ---")
+        print("Your hand:"); Card.print_pretty_cards(player_hand)
+        print("Computer hand:"); Card.print_pretty_cards(computer_hand)
+
+        if p_score < c_score: print("You win")
+        elif c_score < p_score: print("Computer wins")
+        else: print("Chop")
+
+if __name__ == "__main__":
+    game = Game()
+    while True:
+        game.play_hand()
+        if input("\nPlay again? (y/n): ") != "y":
+            break
